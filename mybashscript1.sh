bb66c2f9e1a73e1c3a9e38391f4331a580a5d3a3
vassu <vasanthaeee@gmail.com>
2021-04-06 17:50:00 +0000

    wrong changes
    Revert "in feature branch1"
    
    This reverts commit c8bba06087705efcc703bdb83675da637ff13a4d.

--- mybashscript1.sh
+++ mybashscript1.sh
@@ -1,10 +1,9 @@
-echo "hello there, welcome to developers team"
+echo "hello there"
 uname -a
 ps aux | grep host
 groups
-pwd
 echo "this is second file" > file2.txt
 echo "this is file1.txt" > file1.txt
 cat file1.txt
 cat file2.txt
-ping -c 5 google.com
+
