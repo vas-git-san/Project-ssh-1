d2c6a6342a4d3e51889f55c8d8a2620764807193
vassu <vasanthaeee@gmail.com>
2021-04-06 17:12:06 +0000

    changed to txt extention

--- mybashscript1.sh
+++ mybashscript1.sh
@@ -2,9 +2,8 @@ echo "hello there"
 uname -a
 ps aux | grep host
 groups
-echo "this is first file" > file1.txt
-echo "new feature is added from here"
-cat file1.txt
 echo "this is second file" > file2.txt
+echo "this is file1.txt" > file1.txt
+cat file1.txt
 cat file2.txt
 
