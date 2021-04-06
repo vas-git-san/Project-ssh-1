c8bba06087705efcc703bdb83675da637ff13a4d
vassu <vasanthaeee@gmail.com>
2021-04-06 17:42:58 +0000

    in feature branch1

--- mybashscript1.sh
+++ mybashscript1.sh
@@ -1,9 +1,10 @@
-echo "hello there"
+echo "hello there, welcome to developers team"
 uname -a
 ps aux | grep host
 groups
+pwd
 echo "this is second file" > file2.txt
 echo "this is file1.txt" > file1.txt
 cat file1.txt
 cat file2.txt
-
+ping -c 5 google.com
