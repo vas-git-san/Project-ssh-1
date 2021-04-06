41cf5e93795f017a5a7acc5db4194da32f43984e
vas-git-san <81814886+vas-git-san@users.noreply.github.com>
2021-04-06 12:37:31 -0400

    Update mybashscript1.sh

--- mybashscript1.sh
+++ mybashscript1.sh
@@ -3,11 +3,9 @@ uname -a
 ps aux | grep host
 groups
 cat /etc/passwd | grep user
-for i in {1..9};
-do touch file$i ;
-done;
-echo "this is file1" >> file1.txt
+
+echo "this is file1" > file1.txt
 
 echo "new features adding from here"
 cat file1.txt
-echo "this is file2" >> file2.txt
+echo "this is file2" > file2.txt
