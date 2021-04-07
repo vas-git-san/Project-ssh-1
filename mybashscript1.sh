6247ac4e3208e94f8c13e0beafb356f7c8d037e0
vas-git-san <81814886+vas-git-san@users.noreply.github.com>
2021-04-07 10:01:37 -0400

    pipiline

--- mybashscript1.sh
+++ mybashscript1.sh
@@ -1,9 +1,7 @@
 echo "hello there"
-uname -a
-ps aux | grep host
-groups
 echo "this is second file" > file2.txt
 echo "this is file1.txt" > file1.txt
 cat file1.txt
 cat file2.txt
-
+echo $number1
+echo $number2
